aeab13e4a4f35884354067ed51c1bdecc74a44c2
Yijun Lin <linyjwork@yeah.net>
2020-06-17 04:38:58 +0800

    Lab8 Added.

--- CVLab8/3.py
+++ CVLab8/3.py
@@ -12,14 +12,17 @@ out = cv.VideoWriter('school_bkg.avi', fourcc, out_fps, doublesize)
 
 while video.isOpened():
     print('Reading.')
+
     # 读取下一帧
     (ret, frame) = video.read()
+
     # 如果不能抓取到一帧，说明我们到了视频的结尾
     if not ret:
         break
     fgmask = mog.apply(frame)
     fgmask = cv.threshold(fgmask, 250, 255, cv.THRESH_BINARY)[1]
     fgmask = cv.cvtColor(fgmask, cv.COLOR_GRAY2BGR)
+
     # 显示当前帧
     cv.imshow('frame', fgmask)
     result = cv.hconcat([frame, fgmask])
